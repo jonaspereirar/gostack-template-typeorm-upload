00c756eb4b6db2359ec5f5232cc964ffb8f22862
jonaspereirar <jonaspereirar@gmail.com>
2020-04-27 00:53:07 +0100

    feat. add. cors configured

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -33,7 +33,7 @@ class Transaction {
   })
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
